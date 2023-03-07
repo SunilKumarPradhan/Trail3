b72322cde8d26dea769af46f7dbadb708d7303d8
SunilKumarPradhan <sunilkumarweb47@gmail.com>
2023-03-07 10:06:00 +0530

    at this point the post method is working

--- calculator.js
+++ calculator.js
@@ -1,15 +1,18 @@
+
+
 const express= require("express");
+//const bodyParser= require("body-parser");
 
 const app=express();
-
-// app.get("/",function(req,res){
-//     res.send("Hello World  !")
-// });
+//app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
-    res.send(__dirname+"/index.html")// __dirname is node command used to let the node know the absolute path for the directory to be used. console.log laga ke check kar sakte ho..
+    res.sendFile(__dirname+"/index.html")// __dirname is node command used to let the node know the absolute path for the directory to be used. console.log laga ke check kar sakte ho..
 });
 
+app.post("/",function(req,res){
+  res.send("The Post method works here");
+});
 
 app.listen(3000,function(){
     console.log("Server started at port 3000");
