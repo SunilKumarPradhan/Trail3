4493f90aa29524fe806f9c2efdbe836f0b833ad5
SunilKumarPradhan <sunilkumarweb47@gmail.com>
2023-03-07 16:01:40 +0530

    Send Methods Understood

--- calculator.js
+++ calculator.js
@@ -1,16 +1,17 @@
 
 
 const express= require("express");
-//const bodyParser= require("body-parser");
+const bodyParser= require("body-parser");
 
 const app=express();
-//app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html")// __dirname is node command used to let the node know the absolute path for the directory to be used. console.log laga ke check kar sakte ho..
 });
 
 app.post("/",function(req,res){
+  console.log(req.body);
   res.send("The Post method works here");
 });
 
